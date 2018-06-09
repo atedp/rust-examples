e9b97ac64f0c91786500125c1119aacd0993f23f
atedp <atedplummer@gmail.com>
2018-06-09 16:56:51 -0400

    playing with Result

---
+++ error_handling/target/debug/deps/error_handling-e05ba2ef54a56b5f.d
@@ -0,0 +1,5 @@
+/home/ted/Repos/rust-examples/error_handling/target/debug/deps/error_handling-e05ba2ef54a56b5f: src/main.rs
+
+/home/ted/Repos/rust-examples/error_handling/target/debug/deps/error_handling-e05ba2ef54a56b5f.d: src/main.rs
+
+src/main.rs:
---
+++ error_handling/target/debug/error_handling.d
@@ -0,0 +1,1 @@
+/home/ted/Repos/rust-examples/error_handling/target/debug/error_handling: /home/ted/Repos/rust-examples/error_handling/src/main.rs
