272baae2ec9a2f40e066def32f9bde51a0de2e2f
atedp <atedplummer@gmail.com>
2018-06-09 20:22:40 -0400

    playing with gin

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,34 +1,23 @@
+use std::io;
+use std::io::Read;
 use std::fs::File;
-use std::io::ErrorKind;
 
 fn main() {
-    
-    using_matching();
-
+    read_username_from_file();
 }
 
-
-fn using_matching() {
+fn read_username_from_file() -> Result<String, io::Error>{
     let f = File::open("hello.txt");
 
-    let f = match f {
+    let mut f = match f {
         Ok(file) => file,
-        Err(ref error) if error.kind() == ErrorKind::NotFound => {
-            match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => {
-                    panic!(
-                        "Tried to create file but there was a problem: {:?}",
-                        e
-                    )
-                },
-            }
-        },
-        Err(error) => {
-            panic!(
-                "There was a problem opening the file: {:?}",
-                error
-            )
-        },
+        Err(e) => return Err(e),
     };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
