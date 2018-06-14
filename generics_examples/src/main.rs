d14b98906207cccbfd951c7b5b5b40d70572235d
atedp <atedplummer@gmail.com>
2018-06-13 21:28:15 -0400

    copy and pasted code, DH style! (killme)

--- generics_examples/src/main.rs
+++ generics_examples/src/main.rs
@@ -10,4 +10,16 @@ fn main() {
     }
 
     println!("The largest number is {}", largest);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
 }
