6c38841ac985c9c39312ddb8df3e5b1f1eba810f
atedp <atedplummer@gmail.com>
2018-06-13 20:20:20 -0400

    find the largest number, the slow way

---
+++ generics_examples/target/debug/deps/generics_examples-fa47f85e56a1c86c.d
@@ -0,0 +1,5 @@
+/home/ted/Repos/rust-examples/generics_examples/target/debug/deps/generics_examples-fa47f85e56a1c86c: src/main.rs
+
+/home/ted/Repos/rust-examples/generics_examples/target/debug/deps/generics_examples-fa47f85e56a1c86c.d: src/main.rs
+
+src/main.rs:
---
+++ generics_examples/target/debug/generics_examples.d
@@ -0,0 +1,1 @@
+/home/ted/Repos/rust-examples/generics_examples/target/debug/generics_examples: /home/ted/Repos/rust-examples/generics_examples/src/main.rs
