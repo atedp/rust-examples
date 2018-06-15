432d3c8a5cdff4e745d9d003c17574b11ac7b60f
atedp <atedplummer@gmail.com>
2018-06-15 08:45:01 -0400

    removing duplicate code by making the largest function generic <T>.  DOES NOT COMPILE!!

--- generics_examples/src/main.rs
+++ generics_examples/src/main.rs
@@ -1,16 +1,4 @@
-fn largest_i32(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> char {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
@@ -27,11 +15,11 @@ fn largest_char(list: &[char]) -> char {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 }
