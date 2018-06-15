4851a3011909cc161ba08c73f57becc7d98d6ab2
atedp <atedplummer@gmail.com>
2018-06-15 08:35:39 -0400

    implementing 2 generic data types

--- generics_examples/src/main.rs
+++ generics_examples/src/main.rs
@@ -1,4 +1,4 @@
-fn largest(list: &[i32]) -> i32 {
+fn largest_i32(list: &[i32]) -> i32 {
     let mut largest = list[0];
 
     for &item in list.iter() {
@@ -10,14 +10,28 @@ fn largest(list: &[i32]) -> i32 {
     largest
 }
 
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+
+}
+
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
+    let result = largest_i32(&number_list);
     println!("The largest number is {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 }
