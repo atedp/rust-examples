ffc3610fe371b6b40035605a5040e96ecc3ad89c
atedp <atedplummer@gmail.com>
2018-06-16 08:18:00 -0400

    defined 'Summary' trait, impl'd for NewsArticle & Tweet

---
+++ trait_on_a_type/target/debug/deps/trait_on_a_type-af2e883edd30b466.d
@@ -0,0 +1,5 @@
+/home/ted/Repos/rust-examples/trait_on_a_type/target/debug/deps/trait_on_a_type-af2e883edd30b466: src/main.rs
+
+/home/ted/Repos/rust-examples/trait_on_a_type/target/debug/deps/trait_on_a_type-af2e883edd30b466.d: src/main.rs
+
+src/main.rs:
