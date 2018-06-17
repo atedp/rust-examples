3c9c4093c2ca596497db390347b9b7d041ce8dd4
atedp <atedplummer@gmail.com>
2018-06-17 19:29:09 -0400

    default trait implementation can calls another method (summarize_author)

--- trait_on_a_type/src/main.rs
+++ trait_on_a_type/src/main.rs
@@ -1,6 +1,8 @@
 pub trait Summary {
+    fn summarize_author(&self) -> String;
+    
     fn summarize(&self) -> String {
-        String::from("(Read more...)")
+        format!("(Read more from {}...)", self.summarize_author())
     }
 }
 
@@ -11,11 +13,11 @@ pub struct NewsArticle {
     pub content: String,
 }
 
-impl Summary for NewsArticle {
+//impl Summary for NewsArticle {
     //fn summarize(&self) -> String {
     //    format!("{}, by {} ({})", self.headline, self.author, self.location)
     //}
-}
+//}
 
 pub struct Tweet {
     pub username: String,
@@ -25,8 +27,8 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
 }
 
@@ -41,13 +43,13 @@ fn main() {
 
     println!("1 new tweet: {}", tweet.summarize());
 
-    let article = NewsArticle {
-        headline: String::from("Penguins win the Stanley Cup Championship!"),
-        location: String::from("Pittsburgh, PA, USA"),
-        author: String::from("Iceburgh"),
-        content: String::from("The Pittsburgh Penguins once again are the best 
-        hockey team in the NHL."),
-    };
+    //let article = NewsArticle {
+    //    headline: String::from("Penguins win the Stanley Cup Championship!"),
+    //    location: String::from("Pittsburgh, PA, USA"),
+    //    author: String::from("Iceburgh"),
+    //    content: String::from("The Pittsburgh Penguins once again are the best 
+    //    hockey team in the NHL."),
+    //};
 
-    println!("New article available! {}", article.summarize());
+    //println!("New article available! {}", article.summarize());
 }
