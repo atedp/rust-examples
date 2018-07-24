09a8e39a3f64e44e7ad8bd63a2363e2de4687ebd
atedp <atedplummer@gmail.com>
2018-07-24 17:45:36 -0400

    added ability to read in a .txt file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +10,12 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("Something went wrong reading the file.");
+
+    println!("With text:\n{}", contents);
 }
