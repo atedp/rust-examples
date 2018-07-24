c986c2773ed04e5eb7b828c4d607eff291bc494c
atedp <atedplummer@gmail.com>
2018-07-24 18:01:06 -0400

    extracint a parse_config function from main

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,12 +5,12 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
 
+    
     let mut f = File::open(filename).expect("file not found");
 
     let mut contents = String::new();
@@ -18,4 +18,12 @@ fn main() {
         .expect("Something went wrong reading the file.");
 
     println!("With text:\n{}", contents);
+
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
