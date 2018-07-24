665e591cb941ba22abcbc8c7fb3695ea3f06a10f
atedp <atedplummer@gmail.com>
2018-07-24 17:28:39 -0400

    read argument values, and store in variables

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/ted/Repos/rust-examples/minigrep/target/debug/minigrep: /home/ted/Repos/rust-examples/minigrep/src/main.rs
