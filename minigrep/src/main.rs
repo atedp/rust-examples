43fb81dbd8f976aa0bf10a0f9c48b58a21ef429f
atedp <atedplummer@gmail.com>
2018-07-24 18:06:36 -0400

    groupng configuration  values into a Config struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,13 +5,13 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
     
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
@@ -21,9 +21,15 @@ fn main() {
 
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
+
 }
