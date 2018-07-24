5adb55b2c2fdaaecf5c69b225ea875c359ecdf03
atedp <atedplummer@gmail.com>
2018-07-24 19:42:53 -0400

    added failing search_case_insensitive test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -45,21 +45,41 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(
-            vec!["safe, fast, productive."], 
+            vec!["safe, fast, productive."],
             search(query, contents)
-            );
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUst";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
