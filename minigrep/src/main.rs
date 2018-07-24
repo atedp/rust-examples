3e2e58f79f459210bee10ccdfb8b39fbc191d08d
atedp <atedplummer@gmail.com>
2018-07-24 18:11:49 -0400

    replace fn parse_config with Config::new

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,7 +5,7 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -26,10 +26,11 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config { query, filename }
-
+        Config { query, filename }
+    }
 }
