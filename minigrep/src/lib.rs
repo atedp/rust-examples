688b39a986e0b311175fd3a84338383b7a7dd8a4
atedp <atedplummer@gmail.com>
2018-07-24 19:22:52 -0400

    using the search function in the run function, also removed some prints

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -26,7 +26,9 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -12,10 +12,7 @@ fn main() {
         println!("Problem parsing arguements: {}", err);
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
+    
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
