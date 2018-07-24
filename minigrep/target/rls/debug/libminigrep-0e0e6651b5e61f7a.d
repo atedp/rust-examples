5c11ef90d86156a7ed7e3f416e3747a116fe788f
atedp <atedplummer@gmail.com>
2018-07-24 16:55:46 -0400

    initial commit

---
+++ minigrep/target/rls/debug/libminigrep-0e0e6651b5e61f7a.d
@@ -0,0 +1,1 @@
+/home/ted/Repos/rust-examples/minigrep/target/rls/debug/libminigrep-0e0e6651b5e61f7a.rmeta: /home/ted/Repos/rust-examples/minigrep/src/main.rs
---
+++ minigrep/target/rls/debug/libminigrep.d
@@ -0,0 +1,1 @@
+/home/ted/Repos/rust-examples/minigrep/target/rls/debug/libminigrep.rmeta: /home/ted/Repos/rust-examples/minigrep/src/main.rs
