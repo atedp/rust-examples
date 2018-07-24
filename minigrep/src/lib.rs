bd925975482954b9ff004fcdc6d2e38c8cf8955b
atedp <atedplummer@gmail.com>
2018-07-24 19:05:53 -0400

    wrote a failing test, one_result, for search()

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -30,3 +30,26 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 
     Ok(())
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    vec![]
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+            Rust:
+            safe, fast, productivie. 
+            Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."], 
+            search(query, contents)
+            );
+    }
+}
