b5acbb53d612cf9c1a94715a09e717f08a7177b7
atedp <atedplummer@gmail.com>
2018-07-26 20:23:39 -0400

    made code clearer with some bullshit called iterator adaptors

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -49,15 +49,9 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new(); 
-    
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
