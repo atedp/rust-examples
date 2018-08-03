4a89f6dfaa1722dc99eb40d78050c71fe2f9422f
atedp <atedplummer@gmail.com>
2018-08-02 20:11:49 -0400

    create a blank window

--- game/src/main.rs
+++ game/src/main.rs
@@ -2,4 +2,20 @@ extern crate sdl2;
 
 fn main() {
     let _sdl = sdl2::init().unwrap();
+    let video_subsytem = _sdl.video().unwrap();
+    let window = video_subsytem
+        .window("Game", 900, 700)
+        .resizable()
+        .build()
+        .unwrap();
+    
+    let mut event_pump = _sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => break 'main,
+                _ => {},
+            }
+        }
+    }
 }
