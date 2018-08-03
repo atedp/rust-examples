8c5db12d7c6d23dcfbe3208f4e16ff4287b424b7
atedp <atedplummer@gmail.com>
2018-08-02 20:53:11 -0400

    create opengl context and add a color

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,15 +1,24 @@
 extern crate sdl2;
+extern crate gl;
 
 fn main() {
-    let _sdl = sdl2::init().unwrap();
-    let video_subsytem = _sdl.video().unwrap();
-    let window = video_subsytem
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let window = video_subsystem
         .window("Game", 900, 700)
+        .opengl()
         .resizable()
         .build()
         .unwrap();
+
+    let gl_context = window.gl_create_context().unwrap();
+    let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
     
-    let mut event_pump = _sdl.event_pump().unwrap();
+    unsafe {
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+        }
+
+    let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -17,5 +26,11 @@ fn main() {
                 _ => {},
             }
         }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        
+        window.gl_swap_window();
     }
 }
