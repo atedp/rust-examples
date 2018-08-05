4544853e58ceba9e24e1ca1b87b06105d3ba2036
atedp <atedplummer@gmail.com>
2018-08-05 11:27:45 -0400

    bunch of stuff

---
+++ iridium/src/instruction.rs
@@ -0,0 +1,44 @@
+#[derive(Debug, PartialEq)]
+pub enum Opcode {
+    HLT,
+    IGL,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Instruction {
+    opcode: Opcode
+}
+
+impl Instruction {
+    pub fn new(opcode: Opcode) -> Instruction {
+        Instruction {
+            opcode: opcode
+        }
+    }
+}
+
+impl From<u8> for Opcode {
+    fn from(v: u8) -> Self {
+        match v {
+            0 => return Opcode::HLT,
+            _ => return Opcode::IGL
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_hlt() {
+        let opcode = Opcode::HLT;
+        assert_eq!(opcode, Opcode::HLT);
+    }
+
+    #[test]
+    fn test_create_instruction() {
+        let instruction = Instruction::new(Opcode::HLT);
+        assert_eq!(instruction.opcode, Opcode::HLT);
+    }
+}
--- iridium/src/vm.rs
+++ iridium/src/vm.rs
@@ -1,12 +1,58 @@
+use instruction::{Opcode};
+
+#[derive(Debug)]
 pub struct VM {
-    registers: [i32; 32]
+    registers: [i32; 32],
+    pc: usize,
+    program: Vec<u8>
 }
 
 impl VM {
     pub fn new() -> VM {
         VM {
-            registers: [0; 32]
+            registers: [0; 32],
+            program: vec![],
+            pc: 0,
         }
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            if self.pc >= self.program.len() {
+                break;
+            }
+
+            match self.decode_opcode() {
+                Opcode::HLT => {
+                    println!("HLT encountered");
+                    return;
+                },
+                _ => {
+                    println!("Unrecognized opcode found! Terminating!");
+                    return;
+                }
+            }
+        }
+    }
+
+    fn decode_opcode(&mut self) -> Opcode {
+        let opcode = Opcode::from(self.program[self.pc]);
+        self.pc += 1;
+        return opcode;
+    }
+
+    fn next_8_bits(&mut self) -> u8 {
+        let result = self.program[self.pc];
+        self.pc += 1;
+        return result;
+    }
+
+    fn next_16_bits(&mut self) -> u16 {
+        let result = ((self.program[self.pc] as u16) << 8) | 
+            self.program[self.pc + 1] as u16;
+        self.pc += 2;
+        return result;
+
     }
 }
 
@@ -18,5 +64,31 @@ mod tests {
     fn test_create_vm() {
         let test_vm = VM::new();
         assert_eq!(test_vm.registers[0], 0)
+    }
+
+    #[test]
+    fn test_opcode_hlt() {
+        let mut test_vm = VM::new();
+        let test_bytes = vec![0,0,0,0];
+        test_vm.program = test_bytes;
+        test_vm.run();
+        assert_eq!(test_vm.pc, 1);
+    }
+
+    #[test]
+    fn test_opcode_igl() {
+        let mut test_vm = VM::new();
+        let test_bytes = vec![200,0,0,0];
+        test_vm.program = test_bytes;
+        test_vm.run();
+        assert_eq!(test_vm.pc, 1);
+    }
+
+    #[test]
+    fn test_load_opcode() {
+        let mut test_vm = get_test_vm();
+        test_vm.program = vec![0,0,1,244];  // represents 500 using 2 u8s in little endian format
+        test_vm.run();
+        assert_eq!(test_vm.registers[0], 500);
     }
 }
