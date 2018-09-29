e6bfedca41feb0c0241008e704b4d39de9f017b3
atedp <atedplummer@gmail.com>
2018-09-29 10:16:29 -0400

    document panic

--- documentation/src/lib.rs
+++ documentation/src/lib.rs
@@ -1,11 +1,18 @@
 /// Adds one to the number given
 /// 
 /// # Examples
-/// '''
+/// ```
 /// let five = 5;
 /// 
 /// assert_eq!(6, my_crate::add_one(5));
-/// '''
+/// ```
+/// 
+/// # Panics
+/// ```
+/// let five = 'j';
+/// my_crate::add_one(five)
+/// ```
+/// 
 pub fn add_one(x: i32) -> i32 {
     x + 1
 }
