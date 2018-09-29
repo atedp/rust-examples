56a6f8be3a497b35a245d62fcdccb65bd5120ce1
atedp <atedplummer@gmail.com>
2018-09-29 10:21:03 -0400

    fixed ''' versus ```

--- documentation/src/lib.rs
+++ documentation/src/lib.rs
@@ -4,13 +4,13 @@
 /// ```
 /// let five = 5;
 /// 
-/// assert_eq!(6, my_crate::add_one(5));
+/// assert_eq!(6, documentation::add_one(5));
 /// ```
 /// 
 /// # Panics
 /// ```
 /// let five = 'j';
-/// my_crate::add_one(five)
+/// panic!(documentation::add_one(five))
 /// ```
 /// 
 pub fn add_one(x: i32) -> i32 {
