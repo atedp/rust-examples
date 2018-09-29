42d52cf84058b6415199b28f8f0a0a28a7bd5287
atedp <atedplummer@gmail.com>
2018-09-29 19:53:45 -0400

    wooooooooo it works

--- mutex/src/main.rs
+++ mutex/src/main.rs
@@ -1,11 +1,12 @@
-use std::sync::Mutex;
+use std::sync::{Mutex, Arc};
 use std::thread;
 
 fn main() {
-    let counter = Mutex::new(0);
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
-    for _ in 0..10 {
+    for _ in 0..10{
+        let counter = Arc::clone(&counter);        
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
 
@@ -18,6 +19,6 @@ fn main() {
         handle.join().unwrap();
     }
 
-    println!("Result: {}", counter.lock().unwrap())
+    println!("Result: {}", *counter.lock().unwrap())
 
 }
